b4e68f2d8d79effa3c274ccfa90cf18adec9cba0
easton57 <eseidel1357@gmail.com>
2025-03-15 09:36:58 -0700

    Added a new primeagen based nvim config with some small modifications to fix some seemingly broken packages. vim.g.mapleader = " " vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
    vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
    vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
    
    vim.keymap.set("n", "J", "mzJ`z")
    vim.keymap.set("n", "<C-d>", "<C-d>zz")
    vim.keymap.set("n", "<C-u>", "<C-u>zz")
    vim.keymap.set("n", "n", "nzzzv")
    vim.keymap.set("n", "N", "Nzzzv")
    vim.keymap.set("n", "=ap", "ma=ap'a")
    vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
    
    vim.keymap.set("n", "<leader>vwm", function()
        require("vim-with-me").StartVimWithMe()
    end)
    vim.keymap.set("n", "<leader>svwm", function()
        require("vim-with-me").StopVimWithMe()
    end)
    
    -- greatest remap ever
    vim.keymap.set("x", "<leader>p", [["_dP]])
    
    -- next greatest remap ever : asbjornHaland
    vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
    vim.keymap.set("n", "<leader>Y", [["+Y]])
    
    vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
    
    -- This is going to get me cancelled
    vim.keymap.set("i", "<C-c>", "<Esc>")
    
    vim.keymap.set("n", "Q", "<nop>")
    vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
    
    vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
    vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
    vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
    vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
    
    vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
    vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
    
    vim.keymap.set(
        "n",
        "<leader>ee",
        "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
    )
    
    vim.keymap.set(
        "n",
        "<leader>ea",
        "oassert.NoError(err, \"\")<Esc>F\";a"
    )
    
    vim.keymap.set(
        "n",
        "<leader>ef",
        "oif err != nil {<CR>}<Esc>Olog.Fatalf(\"error: %s\\n\", err.Error())<Esc>jj"
    )
    
    vim.keymap.set(
        "n",
        "<leader>el",
        "oif err != nil {<CR>}<Esc>O.logger.Error(\"error\", \"error\", err)<Esc>F.;i"
    )
    
    vim.keymap.set("n", "<leader>ca", function()
        require("cellular-automaton").start_animation("make_it_rain")
    end)
    
    vim.keymap.set("n", "<leader><leader>", function()
        vim.cmd("so")
    end)

--- nvim/lua/hermitfeather/remap.lua
+++ nvim/lua/hermitfeather/remap.lua
@@ -1,3 +1,4 @@
+
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
@@ -9,6 +10,8 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "=ap", "ma=ap'a")
+vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
 
 vim.keymap.set("n", "<leader>vwm", function()
     require("vim-with-me").StartVimWithMe()
@@ -24,7 +27,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -41,10 +44,34 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+vim.keymap.set(
+    "n",
+    "<leader>ee",
+    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>ea",
+    "oassert.NoError(err, \"\")<Esc>F\";a"
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>ef",
+    "oif err != nil {<CR>}<Esc>Olog.Fatalf(\"error: %s\\n\", err.Error())<Esc>jj"
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>el",
+    "oif err != nil {<CR>}<Esc>O.logger.Error(\"error\", \"error\", err)<Esc>F.;i"
+)
+
+vim.keymap.set("n", "<leader>ca", function()
+    require("cellular-automaton").start_animation("make_it_rain")
+end)
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
-
