7d42053d8eca30519e2cdccbcaf169e3b8f1bd9e
Easton Seidel <eseidel1357@gmail.com>
2025-04-17 11:33:28 -0700

    Added rust, go, and python configurations

--- nvim/lua/hermitfeather/lazy/dap.lua
+++ nvim/lua/hermitfeather/lazy/dap.lua
@@ -37,7 +37,7 @@ return {
         lazy = false,
         config = function()
             local dap = require("dap")
-            dap.set_log_level("DEBUG")
+            dap.set_log_level("ERROR")
 
             vim.keymap.set("n", "<F8>", dap.continue, { desc = "Debug: Continue" })
             vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
@@ -47,10 +47,100 @@ return {
             vim.keymap.set("n", "<leader>B", function()
                 dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
             end, { desc = "Debug: Set Conditional Breakpoint" })
+
+            -- Adapter Configs
+            -- Rust
+            dap.adapters.lldb = {
+                type = 'executable',
+                command = '/usr/bin/lldb-dap', -- adjust as needed, must be absolute path
+                name = 'lldb'
+            }
+            dap.configurations.cpp = {
+            {
+                    name = 'Launch',
+                    type = 'lldb',
+                    request = 'launch',
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}',
+                    stopOnEntry = false,
+                    args = {},
+                },
+            }
+            dap.configurations.rust = dap.configurations.cpp
+            dap.configurations.c = dap.configurations.cpp
+
+            -- Python
+            dap.adapters.python = function(cb, config)
+            if config.request == 'attach' then
+                ---@diagnostic disable-next-line: undefined-field
+                local port = (config.connect or config).port
+                ---@diagnostic disable-next-line: undefined-field
+                local host = (config.connect or config).host or '127.0.0.1'
+                    cb({
+                        type = 'server',
+                        port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+                        host = host,
+                        options = {
+                            source_filetype = 'python',
+                        },
+                    })
+                else
+                    cb({
+                        type = 'executable',
+                        command = '/home/eseidel/.virtualenvs/debugpy/bin/python',
+                        args = { '-m', 'debugpy.adapter' },
+                        options = {
+                            source_filetype = 'python',
+                        },
+                    })
+                end
+            end
+            dap.configurations.python = {
+  {
+            -- The first three options are required by nvim-dap
+            type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+            request = 'launch';
+            name = "Launch file";
+
+            -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+            program = "${file}"; -- This configuration will launch the current file if used.
+            pythonPath = function()
+            -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+                    local cwd = vim.fn.getcwd()
+                        if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+                            return cwd .. '/venv/bin/python'
+                        elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+                            return cwd .. '/.venv/bin/python'
+                        else
+                            return '/usr/bin/python'
+                        end
+                    end;
+                },
+            }
+
+            -- Go
+            dap.adapters.go = {
+                type = 'executable';
+                command = 'node';
+                args = {os.getenv('HOME') .. '/dev/golang/vscode-go/extension/dist/debugAdapter.js'};
+            }
+            dap.configurations.go = {
+            {
+                type = 'go';
+                name = 'Debug';
+                request = 'launch';
+                showLog = false;
+                program = "${file}";
+                    dlvToolPath = vim.fn.exepath('/usr/bin/dlv')  -- Adjust to where delve is installed
+                },
+            }
         end
     },
-
-
     {
         "rcarriga/nvim-dap-ui",
         dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
